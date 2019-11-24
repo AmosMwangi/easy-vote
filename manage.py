9fecaee361fefd93892073fb65ac05b4620faa8c
AmosMwangi <amosmwangi101@gmail.com>
2019-11-24 23:10:25 +0300

    imported migrate

--- manage.py
+++ manage.py
@@ -1,9 +1,8 @@
 from app import create_app,db
-from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager,Shell,Server
+from flask_migrate import Migrate, MigrateCommand
 from app.models import *
 
-# Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
@@ -15,7 +14,6 @@ manager.add_command('server',Server)
 
 @manager.command
 def test():
-    """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
