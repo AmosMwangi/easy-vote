80845a7f3334ddb8cb28e61810cf5e4aeca398f9
AmosMwangi <amosmwangi101@gmail.com>
2019-11-24 23:12:42 +0300

    returned app

--- app/__init__.py
+++ app/__init__.py
@@ -14,10 +14,6 @@ def create_app(config_name):
 
     app = Flask(__name__)
 
-    # configure UploadsSet
-    #configure_uploads(app,photos)
-    
-    # Creating the app configurations
     app.config.from_object(config_options[config_name])
     config_options[config_name].init_app(app)
 
@@ -25,8 +21,6 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-    #mail.init_app(app)
-
 
     # Registering the blueprints
     from .main import main as main_blueprint
@@ -34,12 +28,5 @@ def create_app(config_name):
 
     app.register_blueprint(main_blueprint)
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
-
-    # configure uploads
-    #configure_uploads(app,photos)
-
-    # setting config
-    #from .requests import configure_request
-    #configure_request(app)
 
     return app
