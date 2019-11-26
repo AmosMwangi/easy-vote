a3c62164387d68e0d17e714f3eb265e44956255d
AmosMwangi <amosmwangi101@gmail.com>
2019-11-26 08:24:12 +0300

    added email and password

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY='amos'
+export APP_SETTINGS="config.DevelopmentConfig"
+export DATABASE_URL='postgresql+psycopg2://ermoh:12345@localhost/pich'
+export MAIL_USERNAME='browndwane22@gmail.com'
+export MAIL_PASSWORD='dope fam'
+python3.6 manage.py server
