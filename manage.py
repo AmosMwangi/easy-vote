a56ac4b164ae9fea603e2b1dff5a2df77d4f8dfc
AmosMwangi <amosmwangi101@gmail.com>
2019-11-26 05:42:05 +0300

    Imported shell

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 from app import create_app,db
-from flask_script import Manager,Shell,Server
-from flask_migrate import Migrate, MigrateCommand
 from app.models import *
 
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager,Shell,Server
+
+# Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
@@ -11,16 +13,16 @@ migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 manager.add_command('server',Server)
 
-
 @manager.command
 def test():
+    """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User, Pitch=Pitch,Comment=Comment )
 
 if __name__ == '__main__':
     manager.run()
