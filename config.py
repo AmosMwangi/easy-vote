fd5d672146aa6263d3bd2fe66fbb413aa6ad8a87
AmosMwangi <amosmwangi101@gmail.com>
2019-11-26 05:41:31 +0300

    changed the db

--- config.py
+++ config.py
@@ -2,8 +2,14 @@ import os
 
 class Config:
     SECRET_KEY='amos'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ermoh:12345@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ermoh:12345@localhost/pich'
     UPLOADED_PHOTOS_DEST='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
     @staticmethod
     def init_app(app):
